25b421f8bb297241837b8c25e62c49e1a9b167b3
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-12-28 22:50:03 +0100

    Start on UI, basic WM selector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,200 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
+    widgets::{Block, Borders, Paragraph},
+    Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
+
+mod window_manager_selector;
+use window_manager_selector::{WindowManager, WindowManagerSelectorWidget};
+
+enum InputMode {
+    WindowManager,
+    Username,
+    Password,
+    Normal,
+}
+
+/// App holds the state of the application
+struct App {
+    /// The widget used for selection of the window manager
+    window_manager_widget: WindowManagerSelectorWidget,
+
+    /// Current value of the Username
+    username: String,
+
+    /// Current value of the Password
+    password: String,
+
+    /// Current input mode
+    input_mode: InputMode,
+}
+
+impl Default for App {
+    fn default() -> App {
+        App {
+            window_manager_widget: WindowManagerSelectorWidget::new(vec![
+                WindowManager::new("bspwm", "sxhkd & ; exec bspwm"),
+                WindowManager::new("i3", "/usr/bin/i3"),
+                WindowManager::new("awesome", "/usr/bin/awesome"),
+            ]),
+            username: String::new(),
+            password: String::new(),
+            input_mode: InputMode::Normal,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let app = App::default();
+    let res = run_app(&mut terminal, app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match app.input_mode {
+                InputMode::Normal => match key.code {
+                    KeyCode::Enter => {
+                        app.input_mode = InputMode::WindowManager;
+                    }
+                    KeyCode::Char(x) if x == 'q' => {
+                        return Ok(());
+                    }
+                    _ => {}
+                },
+                InputMode::WindowManager => match key.code {
+                    KeyCode::Enter | KeyCode::Tab => {
+                        app.input_mode = InputMode::Username;
+                    }
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    }
+                    KeyCode::Left => {
+                        app.window_manager_widget.left();
+                    }
+                    KeyCode::Right => {
+                        app.window_manager_widget.right();
+                    }
+                    _ => {}
+                },
+                InputMode::Username => match key.code {
+                    KeyCode::Enter | KeyCode::Tab => {
+                        app.input_mode = InputMode::Password;
+                    }
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    }
+                    KeyCode::Char(c) => {
+                        app.username.push(c);
+                    }
+                    _ => {}
+                },
+                InputMode::Password => match key.code {
+                    KeyCode::Enter | KeyCode::Tab => {
+                        todo!()
+                    }
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    }
+                    KeyCode::Char(c) => {
+                        app.password.push(c);
+                    }
+                    _ => {}
+                },
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .horizontal_margin(2)
+        .vertical_margin(1)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Length(2),
+                Constraint::Length(1),
+                Constraint::Length(2),
+                Constraint::Length(3),
+                Constraint::Length(2),
+                Constraint::Length(3),
+                Constraint::Min(0),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+
+    app.window_manager_widget.render(
+        f,
+        chunks[2],
+        matches!(app.input_mode, InputMode::WindowManager),
+    );
+
+    let username = Paragraph::new(app.username.as_ref())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::WindowManager => Style::default(),
+            InputMode::Username => Style::default().fg(Color::Yellow),
+            InputMode::Password => Style::default(),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Input"));
+    f.render_widget(username, chunks[4]);
+
+    let password = Paragraph::new(app.password.as_ref())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::WindowManager => Style::default(),
+            InputMode::Username => Style::default(),
+            InputMode::Password => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Password"));
+    f.render_widget(password, chunks[6]);
+
+    match app.input_mode {
+        InputMode::Normal | InputMode::WindowManager => {}
+        InputMode::Username => f.set_cursor(
+            chunks[4].x + app.username.width() as u16 + 1,
+            chunks[4].y + 1,
+        ),
+        InputMode::Password => f.set_cursor(
+            chunks[6].x + app.password.width() as u16 + 1,
+            chunks[6].y + 1,
+        ),
+    }
 }
---
+++ src/window_manager_selector.rs
@@ -0,0 +1,365 @@
+use tui::{
+    layout::{Alignment, Rect},
+    style::{Color, Style},
+    terminal::Frame,
+    text::{Span, Spans, Text},
+    widgets::{Block, Paragraph},
+};
+
+const NO_WINDOW_MANAGERS_STRING: &str = "No Window Managers Specified";
+const NO_WINDOW_MANAGERS_STRING_COLOR: [Color; 2] = [Color::LightRed, Color::Red];
+const WINDOW_MANAGER_CUTOFF_WIDTH: usize = 16;
+const PREV_NEXT_ARROWS: [&str; 2] = ["<", ">"];
+const ARROWS_COLOR: [Color; 2] = [Color::DarkGray, Color::Yellow];
+const PREV_NEXT_PADDING: usize = 1;
+const PREV_NEXT_COLOR: [Color; 2] = [Color::DarkGray, Color::DarkGray];
+const WM_PADDING: usize = 2;
+const CURRENT_COLOR: [Color; 2] = [Color::Gray, Color::White];
+
+const MIN_WIDTH: usize = WINDOW_MANAGER_CUTOFF_WIDTH
+    + PREV_NEXT_ARROWS[0].len()
+    + PREV_NEXT_ARROWS[1].len()
+    + PREV_NEXT_PADDING * 2
+    + WM_PADDING * 2;
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct WindowManager {
+    title: String,
+    cmd: String,
+}
+
+#[derive(Debug)]
+struct WindowManagerSelector {
+    selected: Option<usize>,
+    window_managers: Vec<WindowManager>,
+}
+
+pub struct WindowManagerSelectorWidget(WindowManagerSelector);
+
+impl WindowManager {
+    pub fn new(title: impl ToString, cmd: impl ToString) -> WindowManager {
+        let title = title.to_string();
+        let cmd = cmd.to_string();
+
+        Self { title, cmd }
+    }
+}
+
+impl WindowManagerSelector {
+    pub fn new(window_managers: Vec<WindowManager>) -> Self {
+        Self {
+            selected: if window_managers.len() == 0 {
+                None
+            } else {
+                Some(0)
+            },
+            window_managers,
+        }
+    }
+
+    #[inline]
+    fn len(&self) -> usize {
+        self.window_managers.len()
+    }
+
+    #[inline]
+    fn can_move(&self) -> bool {
+        self.selected.map_or(false, |s| s == 0)
+    }
+
+    #[inline]
+    fn next_index(&self, index: usize) -> usize {
+        (index + 1) % self.len()
+    }
+
+    #[inline]
+    fn prev_index(&self, index: usize) -> usize {
+        if index == 0 {
+            self.len() - 1
+        } else {
+            index - 1
+        }
+    }
+
+    fn go_next(&mut self) {
+        self.selected = self.selected.map(|index| self.next_index(index));
+    }
+
+    fn go_prev(&mut self) {
+        self.selected = self.selected.map(|index| self.prev_index(index));
+    }
+
+    fn next(&self) -> Option<&WindowManager> {
+        self.selected.map_or(None, |index| {
+            debug_assert!(self.len() > 0);
+            self.window_managers.get(self.next_index(index))
+        })
+    }
+
+    fn prev(&self) -> Option<&WindowManager> {
+        self.selected.map_or(None, |index| {
+            debug_assert!(self.len() > 0);
+            self.window_managers.get(self.prev_index(index))
+        })
+    }
+
+    pub fn current(&self) -> Option<&WindowManager> {
+        self.selected.map_or(None, |index| {
+            debug_assert!(self.len() > 0);
+            self.window_managers.get(index)
+        })
+    }
+}
+
+impl WindowManagerSelectorWidget {
+    pub fn new(window_managers: Vec<WindowManager>) -> Self {
+        Self(WindowManagerSelector::new(window_managers))
+    }
+
+    fn wm_width(window_manager: &WindowManager) -> usize {
+        // TODO: Take into account not Monospace fonts
+        window_manager.title.len()
+    }
+
+    fn find_width(prev_width: usize, current_width: usize, next_width: usize) -> usize {
+        prev_width
+            + current_width
+            + next_width
+            + PREV_NEXT_ARROWS[0].len()
+            + PREV_NEXT_ARROWS[1].len()
+            + PREV_NEXT_PADDING * 2
+            + WM_PADDING * 2
+    }
+
+    fn do_show_prev(
+        area_width: usize,
+        prev_width: usize,
+        current_width: usize,
+        next_width: Option<usize>,
+    ) -> bool {
+        debug_assert!(area_width >= Self::find_width(0, current_width, 0));
+
+        Self::find_width(prev_width, current_width, next_width.unwrap_or(0)) <= area_width
+    }
+
+    fn do_show_next(
+        area_width: usize,
+        left_width: Option<usize>,
+        middle_width: usize,
+        right_width: usize,
+    ) -> bool {
+        debug_assert!(area_width >= Self::find_width(0, middle_width, 0));
+
+        Self::find_width(left_width.unwrap_or(0), middle_width, right_width) <= area_width
+    }
+
+    pub fn render(
+        &self,
+        frame: &mut Frame<impl tui::backend::Backend>,
+        area: Rect,
+        is_focused: bool,
+    ) {
+        let Self(selector) = self;
+
+        // TODO: Optimize these calls
+        let current = selector.current();
+        let prev = selector.prev();
+        let next = selector.next();
+
+        let is_focused = if is_focused { 1 } else { 0 };
+
+        let mut msg = Vec::with_capacity(
+            // Left + Right +
+            // LeftPad + RightPad +
+            // LeftWM + RightWM +
+            // LeftWMPad + RightWMPad +
+            // MiddleWM = 9
+            9,
+        );
+        if let Some(current) = current {
+            msg.push(Span::styled(
+                PREV_NEXT_ARROWS[0],
+                Style::default().fg(ARROWS_COLOR[is_focused]),
+            )); // Left Arrow
+            msg.push(Span::raw(" ".repeat(PREV_NEXT_PADDING))); // LeftPad
+            if let Some(prev) = prev {
+                if Self::do_show_prev(
+                    area.width.into(),
+                    Self::wm_width(prev),
+                    Self::wm_width(current),
+                    next.map(|next| Self::wm_width(next)),
+                ) {
+                    msg.push(Span::styled(
+                        &prev.title,
+                        Style::default().fg(PREV_NEXT_COLOR[is_focused]),
+                    )); // LeftWM
+                    msg.push(Span::raw(" ".repeat(WM_PADDING))); // LeftWMPad
+                }
+            }
+
+            msg.push(Span::styled(
+                &current.title,
+                Style::default().fg(CURRENT_COLOR[is_focused]),
+            )); // CurrentWM
+
+            if let Some(next) = next {
+                if Self::do_show_next(
+                    area.width.into(),
+                    prev.map(|prev| Self::wm_width(prev)),
+                    Self::wm_width(current),
+                    Self::wm_width(next),
+                ) {
+                    msg.push(Span::raw(" ".repeat(WM_PADDING))); // RightWMPad
+                    msg.push(Span::styled(
+                        &next.title,
+                        Style::default().fg(PREV_NEXT_COLOR[is_focused]),
+                    )); // RightWM
+                }
+            }
+            msg.push(Span::raw(" ".repeat(PREV_NEXT_PADDING))); // RightPad
+            msg.push(Span::styled(PREV_NEXT_ARROWS[1], Style::default().fg(ARROWS_COLOR[is_focused]))); // Right Arrow
+        } else {
+            msg.push(Span::styled(
+                NO_WINDOW_MANAGERS_STRING,
+                Style::default().fg(NO_WINDOW_MANAGERS_STRING_COLOR[is_focused]),
+            ));
+        }
+        let text = Text::from(Spans::from(msg));
+        let widget = Paragraph::new(text)
+            .block(Block::default())
+            .alignment(Alignment::Center);
+
+        frame.render_widget(widget, area);
+    }
+
+    pub fn left(&mut self) {
+        let Self(ref mut selector) = self;
+        selector.go_prev();
+    }
+
+    pub fn right(&mut self) {
+        let Self(ref mut selector) = self;
+        selector.go_next();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod window_manager_selector {
+        use super::*;
+        #[test]
+        fn empty_creation() {
+            let mut selector = WindowManagerSelector::new(vec![]);
+            assert_eq!(selector.current(), None);
+            selector.go_next();
+            assert_eq!(selector.current(), None);
+            selector.go_prev();
+            assert_eq!(selector.current(), None);
+
+            let mut selector = WindowManagerSelector::new(vec![]);
+            assert_eq!(selector.current(), None);
+            selector.go_prev();
+            assert_eq!(selector.current(), None);
+            selector.go_next();
+            assert_eq!(selector.current(), None);
+        }
+
+        #[test]
+        fn single_creation() {
+            let wm = WindowManager::new("abc", "/abc");
+
+            let mut selector = WindowManagerSelector::new(vec![wm.clone()]);
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm));
+
+            let mut selector = WindowManagerSelector::new(vec![wm.clone()]);
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm));
+
+            let mut selector = WindowManagerSelector::new(vec![wm.clone()]);
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm));
+
+            let mut selector = WindowManagerSelector::new(vec![wm.clone()]);
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm));
+        }
+
+        #[test]
+        fn multiple_creation() {
+            let wm1 = WindowManager::new("abc", "/abc");
+            let wm2 = WindowManager::new("def", "/def");
+
+            let mut selector = WindowManagerSelector::new(vec![wm1.clone(), wm2.clone()]);
+            assert_eq!(selector.current(), Some(&wm1));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm2));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm1));
+
+            let mut selector = WindowManagerSelector::new(vec![wm1.clone(), wm2.clone()]);
+            assert_eq!(selector.current(), Some(&wm1));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm2));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm1));
+
+            let mut selector = WindowManagerSelector::new(vec![wm1.clone(), wm2.clone()]);
+            assert_eq!(selector.current(), Some(&wm1));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm2));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm1));
+
+            let mut selector = WindowManagerSelector::new(vec![wm1.clone(), wm2.clone()]);
+            assert_eq!(selector.current(), Some(&wm1));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm2));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm1));
+
+            let wm3 = WindowManager::new("ghi", "/ghi");
+            let wm4 = WindowManager::new("jkl", "/jkl");
+
+            let mut selector = WindowManagerSelector::new(vec![
+                wm1.clone(),
+                wm2.clone(),
+                wm3.clone(),
+                wm4.clone(),
+            ]);
+            assert_eq!(selector.current(), Some(&wm1));
+            selector.go_prev();
+            assert_eq!(selector.current(), Some(&wm4));
+
+            let mut selector = WindowManagerSelector::new(vec![
+                wm1.clone(),
+                wm2.clone(),
+                wm3.clone(),
+                wm4.clone(),
+            ]);
+            assert_eq!(selector.current(), Some(&wm1));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm2));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm3));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm4));
+            selector.go_next();
+            assert_eq!(selector.current(), Some(&wm1));
+        }
+    }
+}
