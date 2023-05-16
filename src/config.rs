a20da6041477d48c4c0e419f74fca090f2ff4cb3
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-05-16 16:34:00 +0200

    Maintenance: Cargo Format 2

--- src/config.rs
+++ src/config.rs
@@ -326,7 +326,7 @@ impl<'de> Deserialize<'de> for SwitcherVisibility {
                     return Err(D::Error::custom(
                         "Invalid key provided to toggle switcher visibility. Only F1-F12 are allowed"
                     ));
-                    
+
                 };
 
                 Self::Keybind(keycode)
