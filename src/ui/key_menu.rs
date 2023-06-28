458602ed4d4119ce5412087578736b8f1d01c3e4
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-06-28 21:40:05 +0200

    Chore: Cargo Clippy

--- src/ui/key_menu.rs
+++ src/ui/key_menu.rs
@@ -3,7 +3,7 @@ use std::process::{Command, Output};
 use crossterm::event::KeyCode;
 use ratatui::layout::{Alignment, Rect};
 use ratatui::style::Style;
-use ratatui::text::{Line, Span, Text};
+use ratatui::text::{Line, Span};
 use ratatui::widgets::Paragraph;
 use ratatui::Frame;
 
