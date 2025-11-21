37a35df211abd3f2c6ca6f2034392e862f51e408
laur <laur.aliste@gmail.com>
2025-11-21 19:47:07 +0100

    feat: update install.sh
    - copy files using install, not cp. its -C (compare) flag ensures
      unchanged files won't be copied, meaning targets' mtimes don't
      get unnecessarily changed; additionally install gives a simple
      option to enforce target file permissions
    - do not allow running as root
    - all in all this should make install.sh more idempotent

--- install.sh
+++ install.sh
@@ -1,18 +1,30 @@
-#! /bin/sh
+#!/usr/bin/env sh
 
-ROOT_CMD=""
+ROOT_CMD=''
 
-if which "doas" > /dev/null 2>&1
-then
-    ROOT_CMD="doas"
-else
-    if which "sudo" > /dev/null 2>&1
-    then
-        ROOT_CMD="sudo";
-    else
-        echo '`sudo` or `doas` needs to be installed';
-        exit 1;
+fail() {
+    echo "$*"
+    exit 1
+}
+
+inst() {
+    if ! "$ROOT_CMD" install -m644 -CT "$1" "$2"; then
+        fail "installing [$1] to [$2] failed"
     fi
+}
+
+if [ "$(id --user)" -eq 0 ]; then
+    fail 'do not run as root'
+elif ! which install > /dev/null 2>&1; then
+    fail '[install] utility needs to be installed'
+fi
+
+if which doas > /dev/null 2>&1; then
+    ROOT_CMD='doas'
+elif which sudo > /dev/null 2>&1; then
+    ROOT_CMD='sudo'
+else
+    fail '[sudo] or [doas] needs to be installed'
 fi
 
 echo 'Lemurs install script'
@@ -20,61 +32,52 @@ echo
 
 # Compile lemurs
 echo 'Compile Lemurs'
-cargo build --release 
-if [ $? -ne 0 ]; then exit 1; fi
+cargo build --release || exit 1
 
 # Move lemurs to /usr/bin
 echo 'Move lemurs into /usr/bin'
-$ROOT_CMD cp -f "target/release/lemurs" "/usr/bin/lemurs"
-if [ $? -ne 0 ]; then exit 1; fi
+inst 'target/release/lemurs' '/usr/bin/lemurs'
 
 # Create lemurs directory
 echo 'Create lemurs configuration directory'
 echo 'NOTE: You still have to move your X or Wayland startup into the proper directories'
-$ROOT_CMD mkdir -p "/etc/lemurs/wms"
-$ROOT_CMD mkdir -p "/etc/lemurs/wayland"
-if [ $? -ne 0 ]; then exit 1; fi
+"$ROOT_CMD" mkdir -p '/etc/lemurs/wms' || exit 1
+"$ROOT_CMD" mkdir -p '/etc/lemurs/wayland' || exit 1
 
 # Copy over configuration file
 echo 'Copy over default configuration'
-$ROOT_CMD cp -f "extra/config.toml" "/etc/lemurs/config.toml"
-if [ $? -ne 0 ]; then exit 1; fi
+inst 'extra/config.toml' '/etc/lemurs/config.toml'
 
 # Copy over xsetup
 echo 'Copy over more files'
-$ROOT_CMD cp -f "extra/xsetup.sh" "/etc/lemurs/xsetup.sh"
-if [ $? -ne 0 ]; then exit 1; fi
+inst 'extra/xsetup.sh' '/etc/lemurs/xsetup.sh'
 
 # Copy over default xinitrc
-if [ -z "${XINITRC}" ]
-then
-    XINITRC="$HOME/.xinitrc"
-fi
+[ -z "$XINITRC" ] && XINITRC="$HOME/.xinitrc"
 
-if [ -f "$XINITRC" ]
-then
+if [ -s "$XINITRC" ]; then
     echo 'Copy over existing xinitrc'
-	$ROOT_CMD cp -f "$XINITRC" "/etc/lemurs/wms/xinitrc"
+    inst "$XINITRC" '/etc/lemurs/wms/xinitrc'
 fi
 
-# Cache the current user
+# Copy over PAM service
 echo 'Copy over PAM service'
-$ROOT_CMD cp -f "extra/lemurs.pam" "/etc/pam.d/lemurs"
+inst 'extra/lemurs.pam' '/etc/pam.d/lemurs'
 
 # Cache the current user
 echo 'Caching the current user'
-$ROOT_CMD /bin/bash -c "echo 'xinitrc\n$USER' > /var/cache/lemurs"
+echo "xinitrc\n$USER" | "$ROOT_CMD" tee /var/cache/lemurs > /dev/null || exit 1
 
 # Disable previous Display Manager
 echo 'Disabling the current display-manager. This might throw an error if no display manager is set up.'
-$ROOT_CMD systemctl disable display-manager.service
+"$ROOT_CMD" systemctl disable display-manager.service || echo "display-manager.service not enabled"
 
 # Copy over systemd service
 echo 'Setting up lemurs service'
-$ROOT_CMD cp -f extra/lemurs.service /usr/lib/systemd/system/lemurs.service
-if [ $? -ne 0 ]; then exit 1; fi
+inst 'extra/lemurs.service' '/usr/lib/systemd/system/lemurs.service'
 
 # Enable lemurs
 echo 'Enable the lemurs service'
-$ROOT_CMD systemctl enable lemurs.service
-if [ $? -ne 0 ]; then exit 1; fi
+"$ROOT_CMD" systemctl enable lemurs.service || exit 1
+
+exit 0
